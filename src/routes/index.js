986d5ca6a7d53754bea6afae4a11e9011d4ae017
arifal <arifaldiosdevelopment@gmail.com>
2025-08-25 20:49:12 +0700

    fix setup app

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,58 +1,58 @@
-const config = require('../../config')
-const healthRoutes = require('./health')
-const authRoutes = require('./auth')
-const coursePerformanceRoutes = require('./course-performance')
-const sasCategorySubjectRoutes = require('./sas-category-subject')
-const sasSummaryRoutes = require('./sas-summary')
-const sasEtlRoutes = require('./etl-student-activity-summary')
-const etlCoursePerformanceRoutes = require('./etl-course-performance')
-const celoeApiRoutes = require('./celoe-api')
+const config = require("../../config");
+const healthRoutes = require("./health");
+const authRoutes = require("./auth");
+const coursePerformanceRoutes = require("./course-performance");
+const sasCategorySubjectRoutes = require("./sas-category-subject");
+const sasSummaryRoutes = require("./sas-summary");
+const sasEtlRoutes = require("./etl-student-activity-summary");
+const etlCoursePerformanceRoutes = require("./etl-course-performance");
+const celoeApiRoutes = require("./celoe-api");
 
 // Combine all routes
 const routes = [
   // Health check routes (no prefix needed)
-  ...healthRoutes.map(route => {
+  ...healthRoutes.map((route) => {
     return {
       ...route,
-      path: `${config.api.prefix}/health${route.path}`
-    }
+      path: `${config.api.prefix}/health${route.path}`,
+    };
   }),
 
   // API routes with prefix
-  ...authRoutes.map(route => ({
+  ...authRoutes.map((route) => ({
     ...route,
-    path: `${config.api.prefix}/auth${route.path}`
+    path: `${config.api.prefix}/auth${route.path}`,
   })),
 
-  ...coursePerformanceRoutes.map(route => ({
+  ...coursePerformanceRoutes.map((route) => ({
     ...route,
-    path: `${config.api.prefix}/cp${route.path}`
+    path: `${config.api.prefix}/cp${route.path}`,
   })),
 
-  ...sasCategorySubjectRoutes.map(route => ({
+  ...sasCategorySubjectRoutes.map((route) => ({
     ...route,
-    path: `${config.api.prefix}/sas-category-subject${route.path}`
+    path: `${config.api.prefix}/sas-category-subject${route.path}`,
   })),
 
-  ...sasEtlRoutes.map(route => ({
+  ...sasEtlRoutes.map((route) => ({
     ...route,
-    path: `${config.api.prefix}/etl-sas${route.path}`
+    path: `${config.api.prefix}/etl-sas${route.path}`,
   })),
 
-  ...celoeApiRoutes.map(route => ({
+  ...celoeApiRoutes.map((route) => ({
     ...route,
-    path: `${config.api.prefix}${config.celoeapi.prefix}${route.path}`
+    path: `${config.api.prefix}${config.celoeapi.prefix}${route.path}`,
   })),
 
-  ...etlCoursePerformanceRoutes.map(route => ({
+  ...etlCoursePerformanceRoutes.map((route) => ({
     ...route,
-    path: `${config.api.prefix}/etl-cp${route.path}`
+    path: `${config.api.prefix}/etl-cp${route.path}`,
   })),
 
-  ...sasSummaryRoutes.map(route => ({
+  ...sasSummaryRoutes.map((route) => ({
     ...route,
-    path: `${config.api.prefix}/sas/summary${route.path}`
-  }))
-]
+    path: `${config.api.prefix}/sas/summary${route.path}`,
+  })),
+];
 
-module.exports = routes
+module.exports = routes;
